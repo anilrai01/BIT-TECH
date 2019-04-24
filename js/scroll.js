1834096ab45d49ede4a2019a92de99d17c0bc859
anilrai01 <anil.rai01041998@gmail.com>
2019-04-24 18:45:58 +0545

    Adjusted Indentation and Banner

--- js/scroll.js
+++ js/scroll.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 (function($) {
 
   // Add smooth scrolling to all links in navbar
-  $(".navbar a, #head .link a").on('click', function(event) {
+  $(".navbar a").on('click', function(event) {
 
     var hash = this.hash;
     if (hash) {
@@ -50,56 +49,3 @@
 
 
 })(jQuery);
-=======
-(function($) {
-
-  // Add smooth scrolling to all links in navbar
-  $(".navbar a, #head .link a").on('click', function(event) {
-
-    var hash = this.hash;
-    if (hash) {
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 900, function() {
-        window.location.hash = hash;
-      });
-    }
-
-  });
-
-
-  $(".navbar-collapse a").on('click', function() {
-    $(".navbar-collapse.collapse").removeClass('in');
-  });
-
-  //jQuery to collapse the navbar on scroll
-  $(window).scroll(function() {
-    if ($(".navbar-default").offset().top > 20){
-      $(".fixed-top").addClass("top-nav-collapse");
-    } else {
-      $(".fixed-top").removeClass("top-nav-collapse");
-    }
-  });
-
-  $(window).scroll(function() {
-    if ($(".navbar-default").offset().top > 20){
-      $("#head nav ul li").addClass("changeFont");
-    } else {
-      $("#head nav ul li a").removeClass("changeFont");
-    }
-  });
-
-  //Active add and remove
-  $(document).on('click','#head ul li a', function(){
-      $(this).addClass('active').siblings().removeClass('active')
-  })
-
-  //Portfolio
-  $(document).on('click', '.portfolio-menu ul li', function(){
-    $(this).addClass('filter-active').siblings().removeClass('filter-active')
-  })
-
-
-})(jQuery);
->>>>>>> 4eec6ccef72140a3eb750ba0faa4d5cabf5c1557
