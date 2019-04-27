16bc585dfe7e815d72f8668c4bcb06fdc1d880ac
anil.rai01 <anil.rai01041998@gmail.com>
2019-04-27 15:03:43 +0545

    Finally fixed Nav Bug

--- js/scroll.js
+++ js/scroll.js
@@ -1,4 +1,7 @@
 (function($) {
+	"use strict";
+  var nav = $('nav');
+  var navHeight = nav.outerHeight();
 
   // Add smooth scrolling to all links in navbar
   $(".navbar a").on('click', function(event) {
@@ -8,7 +11,7 @@
       event.preventDefault();
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 900, function() {
+      }, 1500, function() {
         window.location.hash = hash;
       });
     }
@@ -16,9 +19,9 @@
   });
 
 
-  $(".navbar-collapse a").on('click', function() {
-    $(".navbar-collapse.collapse").removeClass('in');
-  });
+  // $(".navbar-collapse a").on('click', function() {
+  //   $(".navbar-collapse.collapse").removeClass('in');
+  // });
 
   //jQuery to collapse the navbar on scroll
   $(window).scroll(function() {
@@ -29,23 +32,30 @@
     }
   });
 
-  $(window).scroll(function() {
-    if ($(".navbar-default").offset().top > 20){
-      $("#head nav ul li").addClass("changeFont");
-    } else {
-      $("#head nav ul li a").removeClass("changeFont");
-    }
-  });
+  // $(window).scroll(function() {
+  //   if ($(".navbar-default").offset().top > 20){
+  //     $("#head nav ul li").addClass("changeFont");
+  //   } else {
+  //     $("#head nav ul li a").removeClass("changeFont");
+  //   }
+  // });
 
   //Active add and remove
   $(document).on('click','#head ul li a', function(){
       $(this).addClass('active').siblings().removeClass('active')
-  })
+  });
 
   //Portfolio
   $(document).on('click', '.portfolio-menu ul li', function(){
     $(this).addClass('filter-active').siblings().removeClass('filter-active')
-  })
+  });
 
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+		target: '#mainNav',
+		offset: navHeight
+	});
 
 })(jQuery);
+
+// console.log(navHeight);
